61fa167dc829b89eb59a5f9b7e3e8cbdef0be8d6
Flávia Fogaça <flaviamariafogaca@gmail.com>
2024-08-12 09:36:59 -0400

    ajustes no formulario

--- src/javascript/script.js
+++ src/javascript/script.js
@@ -13,13 +13,29 @@ document.getElementById("region").addEventListener("change", function() {
 
 document.getElementById('games-button').addEventListener('click', function() {
     var container = document.getElementById('games-container');
-    var button = document.getElementById('games-button');
     container.style.display = container.style.display === 'block' ? 'none' : 'block';
-    button.classList.toggle('active');
 });
 
 document.getElementById('registration-form').addEventListener('submit', function(event) {
     event.preventDefault(); // Impede o envio real do formulário para fins de demonstração
-    alert('Formulário enviado!'); // Teste para verificar se o evento está sendo capturado
+    alert('Formulário enviado! Você receberá um e-mail de confirmação!'); // Teste para verificar se o evento está sendo capturado
     document.getElementById('form-message').style.display = 'block';
 });
+
+function verificarRegiao() {
+    const regiao = document.getElementById('region').value;
+    const outraRegiao = document.getElementById('other-region');
+    if (regiao === "outro") {
+        outraRegiao.style.display = "block";
+        outraRegiao.required = true;
+    } else {
+        outraRegiao.style.display = "none";
+        outraRegiao.required = false;
+    }
+}
+
+// Função para concluir a seleção de jogos
+function concluirSelecao() {
+    const gamesContainer = document.getElementById('games-container');
+    gamesContainer.style.display = 'none';
+}
