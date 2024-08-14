87d887420eca0617d03a2472bce8c9f91c3e679f
Flávia Fogaça <flaviamariafogaca@gmail.com>
2024-08-14 11:53:31 -0400

    altera o texto do email

--- backend/index.js
+++ backend/index.js
@@ -65,8 +65,9 @@ app.post('/submit', (req, res) => {
                 <p>Seu cupom é <strong>FURIA10</strong>.</p>
                 <p>Por aqui, sempre compartilharemos em primeira mão as novidades sobre os nossos produtos, conteúdos exclusivos, informações de campeonatos, jogos e muito mais!</p>
                 <p>Vamos movimentar e conectar nossa comunidade! Siga-nos nas redes sociais e nos vemos em Summoner's Rift!</p>
+                <br>
                 <p>Um abraço,</p>
-                ,<br>Time Furia</p>
+                <p>Time Furia</p>
             </body>
             </html>
         `,
